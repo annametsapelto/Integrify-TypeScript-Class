e7f33bcab221e5552bcdd5cfaa3ca709c7189d38
Anna Metsäpelto <anna.metsapelto@gmail.com>
2022-12-19 12:38:27 +0200

    Finish bank

--- src/types/bank.ts
+++ src/types/bank.ts
@@ -1,4 +1,6 @@
 import { Branch } from './branch';
+import { Customer } from './customer';
+import { Transaction } from './transactions';
 
 export class Bank {
     private name: string;
@@ -9,9 +11,24 @@ export class Bank {
         this.branches = branches;
     }
 
-    addBranch(branch: Branch) {
-        //code here
-        return true;
+    getName() {
+        return this.name;
+    }
+
+    getBranches() {
+        return this.branches;
+    }
+
+    addBranch(branch: Branch): boolean {
+        let branchAdded: boolean;
+        this.getBranches().forEach((currentBranch) => {
+            if (currentBranch.getName() === branch.getName()) {
+                branchAdded = false;
+                return;
+            }})
+        this.getBranches().push(branch);
+        branchAdded = true;
+        return branchAdded;
     }
 
     findBranchByName(branchName: string) {       
@@ -27,5 +44,39 @@ export class Bank {
             branchFound = false;}
         return branchFound;
     }
+    
+    addCustomer(branch: Branch, customer: Customer): boolean {
+        return branch.addCustomer(customer);
+    }
 
+    addCustomerTransaction(branch: Branch, customerId: string, transaction: number): boolean {
+        return branch.addCustomerTransaction(customerId, transaction);
+    }
+
+    listCustomers(branch: Branch, listCustomerTransactions: boolean): boolean {
+        const branchFound: boolean = this.checkBranch(branch);
+
+        if (branchFound) {
+            const customers: Customer[] = branch.getCustomers();
+            customers.forEach((customer) => {
+                console.log(`Customer name: ${customer.getName()}`);
+
+                if (listCustomerTransactions) {
+                    if (customer.getTransactions().length > 0 ) {
+                        console.log(`${customer.getName()}'s transactions in ${branch.getName()}: `);
+
+                        const transactions: Transaction[] = customer.getTransactions();
+
+                        transactions.forEach((action) => {
+                            console.log(`Amount: ${action.amount}, date: ${action.date}`);
+                        })
+                    }
+                } else {
+                    console.log(`${customer.getName()} has no transaction in ${branch.getName()}}`);
+                }
+            })
+        }
+
+        return branchFound;
+    }
 }
